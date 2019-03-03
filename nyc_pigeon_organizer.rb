cb84e22cd122840988439eed7e5a74cab03f2def
Daniel Ko <dankomong@gmail.com>
2019-03-03 21:43:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,6 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, values|
     values.each do |att, names|
       names.each do |name|
+        # imo, line 9 is the most important line as it helps distributes names to its respective attribute
         new_pigeon[name] ||= {}
         new_pigeon[name][key] ||= []
         new_pigeon[name][key].push(att.to_s)
