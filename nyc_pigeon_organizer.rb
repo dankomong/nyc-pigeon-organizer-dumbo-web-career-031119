b92a06dab988577d0ed95a75f55226544eae1466
Daniel Ko <dankomong@gmail.com>
2019-03-03 21:35:12 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,11 @@ def nyc_pigeon_organizer(data)
   colors = []
   genders = []
   livess = []
+  name_sub = ""
   data.each do |key, values|
     values.each do |color, names|
       names.each do |name|
+        name_sub = name
         
       end
     end
