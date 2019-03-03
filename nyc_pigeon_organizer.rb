ed6c30096778e06f109ac467e4a23318cd0f02fa
Daniel Ko <dankomong@gmail.com>
2019-03-03 21:41:57 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,11 +3,11 @@ def nyc_pigeon_organizer(data)
   new_pigeon = {}
   name_sub = ""
   data.each do |key, values|
-    values.each do |color, names|
+    values.each do |att, names|
       names.each do |name|
         new_pigeon[name] ||= {}
         new_pigeon[name][key] ||= []
-        new_pigeon[name][key].push(color.to_s)
+        new_pigeon[name][key].push(att.to_s)
       end
     end
   end
